268e9e32965945fc0fc14671541e0845006b0520
Ekaterina Lukovich <elukovich@mail.ru>
2023-06-25 22:57:21 +0300

    Task3

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        some_student = f'Студенты:\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: 9.9\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.courses_in_progress}'
+        return some_student
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -32,6 +36,10 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        some_lecturer = f'Лекторы:\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: 9.9'
+        return some_lecturer
+
 
 
 class Reviewer(Mentor):
@@ -43,6 +51,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        some_reviewer = f'Проверяющие:\nИмя: {self.name}\nФамилия: {self.surname}'
+        return some_reviewer
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -62,5 +74,10 @@ best_student.rate_lec(good_lecturer, 'Python', 10)
 best_student.rate_lec(good_lecturer, 'Python', 10)
 best_student.rate_lec(good_lecturer, 'Python', 10)
 
-print(best_student.grades)
-print(best_student.grades)
+# print(best_student.grades)
+# print(good_lecturer.grades)
+print(best_student)
+print()
+print(good_lecturer)
+print()
+print(cool_reviewer)
